cdbb75ccad5e581eef1cb74f44f098273266e4ff
Chris Goodmacher <cgoodmac@gmail.com>
2012-11-29 16:38:12 -0500

    first attempt at function

--- app/controllers/subway_controller.rb
+++ app/controllers/subway_controller.rb
@@ -4,11 +4,30 @@ class SubwayController < ApplicationController
   end
 
   def entrances
+
+    Entrance.delete_all
+
     data = HTTParty.get('http://data.cityofnewyork.us/api/views/drex-xx56/rows.json')
     json = JSON(data.body)
     entrance_array = json["data"]
-    
 
-    binding.pry
+    entrance_array.each do |e|
+      lat = e[9][1]
+      long = e[9][2]
+      name = e[10]
+      lines_string = e[12]
+
+      e1 = Entrance.create(:name => name, :lat => lat, :long => long, :line => lines_string)
+
+      lines_array = lines_string.split('-')
+
+      lines_array.each do |l|
+        line = Line.where(:name => l).first
+        line.entrances << e1
+      end
+
+    end
+
   end
+
 end  
