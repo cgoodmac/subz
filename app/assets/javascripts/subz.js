eeed1ba81703595721b64c123efc747421b69e65
Dustin Coates <dustin@dcoates.com>
2012-11-29 17:09:39 -0500

    more styling

--- app/assets/javascripts/subz.js
+++ app/assets/javascripts/subz.js
@@ -5,7 +5,7 @@ $(function() {
 	function display_map () {
 		var mapOptions = {
 			center: new google.maps.LatLng("40.636375333000046","-73.9946119999999"),
-			zoom: 7,
+			zoom: 11,
 			mapTypeId: google.maps.MapTypeId.ROADMAP
 		}
 
@@ -14,5 +14,9 @@ $(function() {
 	}
 
 	display_map();
-
+	$('.sidebar').click(hideSidebar)
 });
+
+	function hideSidebar () {
+		$('.sidebar').toggleClass('hidden');
+	}
